c3a56757e841319575f5a51dae5c80975e877328
Powell <preston.powell@nebo.edu>
2022-08-29 14:36:27 -0600

    Updated player controller

--- First Project B1/Assets/Scripts/PlayerController.cs
+++ First Project B1/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Access Mod, Data Type, Name ;
     public float speed; // set speed value
-    public float rotSpeed; // Rotation
+    public float rotSpeed; // Rotation speed
     public float hInput; // horizontal input
     public float vInput; //vertical input
     
     // Update is called once per frame
     void Update()
     {
+        // Collect Input values from keyboard
         hInput = Input.GetAxis("Horizontal");
         vInput = Input.GetAxis("Vertical");
 
-        transform.Rotate(Vector3.up, rotSpeed * hInput * Time.deltaTime);// Left and Right movement
+        // Move the character around
+        transform.Rotate(Vector3.up, rotSpeed * hInput * Time.deltaTime);        
+        //Rotate Left and Right 
 
-        transform.Translate(Vector3.forward * speed * vInput * Time.deltaTime);//Forward and Backward movement
+        transform.Translate(Vector3.forward * speed * vInput * Time.deltaTime);//Forward and Backward movement        
     }
 }
